122f19360d410c5d78cb273d8efbbfc916d9baf8
kloothommel14 <62178461+kloothommel14@users.noreply.github.com>
2020-12-14 20:42:25 +0100

    added another mode. works safer.

--- main.c
+++ main.c
@@ -1,38 +1,67 @@
 #include <stdio.h>
-#include <windowsx.h>
 #include <rpc.h>
-#include <zconf.h>
 
 #define ANSI_COLOR_GREEN   "\x1b[32m"
+int random(int min, int max){
+    return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+}
 
-int click(int ms){
+int click(){
+    int ms = 200;
+    printf("enter cps please");
+    scanf ("%d",&ms);
+    printf("cps: %d", ms);
     boolean click = FALSE;
     if (ms < 10){
         ms = 10;
     }
     while (1==1){
         if(GetAsyncKeyState(VK_LBUTTON) > 0){
-            printf("false");
             click = FALSE;
         }
         if(GetAsyncKeyState(VK_LBUTTON) < 0){
-            printf("true");
             click = TRUE;
         }
         if(click == TRUE){
-            printf("pressed");
             mouse_event(MOUSEEVENTF_LEFTUP,0,0,0,0);
+            Sleep(random(10,30));
             mouse_event(MOUSEEVENTF_LEFTDOWN,0,0,0,0);
             Sleep(1000/ms);
         }
     }
 }
+int clickRand(){
+    int msMax = 200;
+    int msMin = 200;
+    printf("enter max delay in milliseconds");
 
+    scanf("%d", msMax);
+    printf("enter min delay in milliseconds");
+    scanf("%d", msMin);
+
+    boolean click = FALSE;
+
+
+    while (1==1){
+        if(GetAsyncKeyState(VK_LBUTTON) > 0){
+            click = FALSE;
+        }
+        if(GetAsyncKeyState(VK_LBUTTON) < 0){
+            click = TRUE;
+        }
+        if(click == TRUE){
+            mouse_event(MOUSEEVENTF_LEFTUP,0,0,0,0);
+            Sleep(random(10,30));
+            mouse_event(MOUSEEVENTF_LEFTDOWN,0,0,0,0);
+            Sleep(random(msMin,msMax));
+        }
+    }
+}
 int main() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
     WORD saved_attributes;
-    int msdelay = 200;
+
     GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
     saved_attributes = consoleInfo.wAttributes;
     SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
@@ -45,13 +74,17 @@ int main() {
            "| (___) || )  \\  |___) (___    | (____/\\| (____/\\___) (___| (____/\\|  /  \\ \\| (____/\\| ) \\ \\__\n"
            "(_______)|/    )_)\\_______/    (_______/(_______/\\_______/(_______/|_/    \\/(_______/|/   \\__/\n");
     printf("\n");
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
-    printf("DO NOT CLICK IN THE CONSOLE, YOU CAN PUT IN INPUT WITHOUT CLICKING.");
-    printf("\n");
     SetConsoleTextAttribute(hConsole, saved_attributes);
-    printf("enter CPS please.\n");
-    scanf ("%d",&msdelay);
-    printf("cps: %d", msdelay);
-    click(msdelay);
+    printf("enter 1 for a not random autoclicker, enter 2 for a random autoclicker.\n");
+    int selection = 0;
+    scanf ("%d",&selection);
+    if(selection==1){
+        click();
+    }else if(selection==2){
+        clickRand();
+    }else{
+        printf("invalid option. rerun and provide valid options");
+        return 0;
+    }
     return 0;
 }
